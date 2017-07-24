775e34a53779dbcd5c89179e0a9847ed9c20fca2
Alexey Vedensky <avedensky@gmail.com>
2017-07-24 22:34:48 +0300

    Add:allow host in setting.py config for all

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&d(78bysw3bmpi0#=8-px$&i&ao4qm=&tx=mge)#fkx55^nqw)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
