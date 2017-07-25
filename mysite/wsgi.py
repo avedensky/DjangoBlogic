b1c6811688e44e225daf253fdacba3f326027823
Alexey Vedensky <avedensky@gmail.com>
2017-07-25 18:54:46 +0300

    Add:Config wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/avedensky/DjangoBlogic'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
